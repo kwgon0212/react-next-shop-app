0ccb68a1f9649e0c87cb02eb1fe7d1cd466f4aa3
김우곤 <kwgon0102@gmail.com>
2024-12-07 17:27:06 +0900

    chore: 외부 이미지 사용을 위해 도메인 추가

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "src/styles")],
   },
+  images: {
+    domains: ["images.unsplash.com"],
+  },
 };
 
 module.exports = nextConfig;
