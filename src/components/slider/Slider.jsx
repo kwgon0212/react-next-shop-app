66fb8c2ef376cd439dd77250d1ed514e2ab1357f
김우곤 <kwgon0102@gmail.com>
2024-12-07 17:27:34 +0900

    feat: 메인페이지 이미지 슬라이더 UI 구성

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+"use client";
+
+import React, { useCallback, useEffect, useState } from "react";
+import sliderData from "./SliderData";
+import styles from "./Slider.module.scss";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+import Image from "next/image";
 
 const Slider = () => {
-  return <div>Slider</div>;
+  const [currSlide, setCurrSlide] = useState(0);
+  const sliderLength = sliderData.length;
+
+  const intervalTime = 5000;
+
+  const nextSlide = useCallback(() => {
+    setCurrSlide(currSlide === sliderLength - 1 ? 0 : currSlide + 1);
+  }, [currSlide, sliderLength]);
+
+  const prevSlide = useCallback(() => {
+    setCurrSlide(currSlide === 0 ? sliderLength - 1 : currSlide - 1);
+  }, [currSlide, sliderLength]);
+
+  useEffect(() => {
+    const interval = setInterval(nextSlide, intervalTime);
+
+    return () => clearInterval(interval);
+  }, [nextSlide]);
+
+  return (
+    <div className={styles.slider}>
+      <AiOutlineArrowLeft
+        className={`${styles.arrow} ${styles.prev}`}
+        onClick={prevSlide}
+      />
+      <AiOutlineArrowRight
+        className={`${styles.arrow} ${styles.next}`}
+        onClick={nextSlide}
+      />
+      {sliderData.map((slider, index) => {
+        const { image, heading } = slider;
+        return (
+          <div
+            key={heading}
+            className={`${styles.slide} ${
+              index === currSlide && `${styles.current}`
+            }`}
+          >
+            {index === currSlide && <Image src={image} alt={heading} fill />}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Slider;
---
+++ src/components/slider/SliderData.js
@@ -0,0 +1,22 @@
+const sliderData = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1721332153521-120cb0cd02d9?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8",
+    heading: "title 1",
+    desc: "description 1",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1733411676590-951bda1c25b8?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8",
+    heading: "title 2",
+    desc: "description 2",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1733299704493-9aa9c608778a?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw5fHx8ZW58MHx8fHx8",
+    heading: "title 3",
+    desc: "description 3",
+  },
+];
+
+export default sliderData;
